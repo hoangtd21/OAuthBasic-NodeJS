4911936b9dd6dbbdd7a8b065cff1084fe71d1b55
hoangtd21 <61835853+hoangtd21@users.noreply.github.com>
2020-11-17 20:58:09 +0700

    Update key.js
    Update

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     google:{
         clientID:'693808011184-9sqpgmu0jrt6fiv40kfcuuqc8d5amuvm.apps.googleusercontent.com',
-        clientSecret:'s4E8FaCzbLlcRU5KNjS3mxQc'
+        clientSecret:'client_Secret' //s4E8FaCzbLlcRU5KNjS3mxQc
     },
     mongodb:{
         dbURI:'mongodb+srv://hoangtd:hoangtd21@cluster0.jvsob.mongodb.net/OAuth-NodeJS'
